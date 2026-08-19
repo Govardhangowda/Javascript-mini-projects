46efb12fa85a637d9d5a5fba1d3b657a259b73e2
Govardhan <66202233+Govardhangowda@users.noreply.github.com>
2026-08-19 11:08:00 +0530

    final version of rock paper scissors
    contains autoplay, plays with just pressing rock, paper, scissors

---
+++ Rock Paper Scissors-Graphical/final-rock-paper-scissors/scripts/12-rock-paper-scissors.js
@@ -0,0 +1,140 @@
+ let score = JSON.parse(localStorage.getItem('score')) || {
+      wins: 0,
+      loses: 0,
+      ties: 0
+   };
+
+    function updatescoreelement(){
+      document.querySelector('.js-score').innerHTML=`Wins:${score.wins} Loses:${score.loses} Ties:${score.ties}`;
+      }
+    updatescoreelement();
+   
+    
+    console.log(JSON.parse(localStorage.getItem('score')));
+
+    let isAutoPlaying=false;
+    let intervalid;
+
+    function autoplay(){
+      if(isAutoPlaying===false){
+        intervalid= setInterval(function(){
+        const playermove=pickcomputermove();
+        determination(playermove);
+      },1000);
+      isAutoPlaying=true;
+      } else{
+        clearInterval(intervalid);
+        isAutoPlaying=false;
+      }
+      
+    }
+
+    document.querySelector('.js-rock-button').addEventListener('click',() => {
+      determination('Rock');
+    })
+
+    document.querySelector('.js-scissors-button').addEventListener('click',() => {
+      determination('Scissors');
+    })
+
+    document.querySelector('.js-paper-button').addEventListener('click',() => {
+      determination('Paper');
+    })
+
+    document.querySelector('.js-reset').addEventListener('click',() =>{
+      score.wins=0;
+      score.loses=0;
+      score.ties=0;
+      localStorage.removeItem('score');
+      updatescoreelement();
+    })
+
+    document.querySelector('.js-autoplay').addEventListener('click',() =>{
+      autoplay();
+    })
+
+
+    document.body.addEventListener('keydown',(event) =>{
+      if (event.key===('r'||'R')){
+        determination('Rock');
+      }else if(event.key===('s' || 'S')){
+        determination('Scissors');
+      }else if(event.key===('p' || 'P')){
+        determination('Paper');
+      }
+  }
+  );
+
+
+    function determination(umove) {
+      const cmove = pickcomputermove();  // declare once
+      let result = '';                   // declare once
+
+      if (umove === 'Scissors') {
+        if (cmove === 'Rock') {
+          result = 'You Lose';
+        } else if (cmove === 'Paper') {
+          result = 'You Win';
+        } else {
+          result = 'Tie';
+        }
+      } else if (umove === 'Paper') {
+        if (cmove === 'Rock') {
+          result = 'You Win';
+        } else if (cmove === 'Paper') {
+          result = 'Tie';
+        } else {
+          result = 'You Lose';
+        }
+      } else if (umove === 'Rock') {
+        if (cmove === 'Rock') {
+          result = 'Tie';
+        } else if (cmove === 'Paper') {
+          result = 'You Lose';
+        } else {
+          result = 'You Win';
+        }
+      }
+
+      if(result === 'You Win'){
+        score.wins++;
+      }else if(result === 'You Lose'){
+        score.loses++;
+      }else{
+        score.ties++;
+      }
+
+      localStorage.setItem('score',JSON.stringify(score));
+
+      updatescoreelement();
+
+      document.querySelector('.js-result').innerHTML=`${result}`;
+
+      document.querySelector('.js-moves').innerHTML=`You 
+    <img src="images/${umove}-emoji.png" class="move-icon" alt="">
+    <img src="images/${cmove}-emoji.png"  class="move-icon" alt="">
+    Computer`;
+
+      
+      
+      
+    }
+
+
+    function pickcomputermove(){
+      let computerMOve='';
+      const randomNumber = Math.random();
+
+      if(randomNumber>=0 && randomNumber<(1/3)){
+        computerMOve='Rock';
+      }
+      else if(randomNumber>=(1/3) && randomNumber<(2/3))
+      {
+        computerMOve='Paper';
+      } 
+      else
+      {
+        computerMOve='Scissors';
+      }
+      return computerMOve;
+    }
